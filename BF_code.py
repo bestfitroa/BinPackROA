6abe765a3f72e8166190511d5a412d780ab2ceae
bestfitroa <138655432+bestfitroa@users.noreply.github.com>
2023-07-07 10:36:01 +0530

    Update BF_code.py

--- BF_code.py
+++ BF_code.py
@@ -10,20 +10,11 @@ one = 1 + 1e-14 ## custom "1" to prevent rounding errors.
 
 
 
-def compare(item1, item2):                      # custom comparator for bins, so as to sort bin configurations in decreasing order of load.
-    return -sum(item1) + sum(item2)
+def compare(bin1, bin2):                      # custom comparator for bins, so as to sort bin configurations in decreasing order of load.
+    return -sum(bin1) + sum(bin2)
 
 
-def strconv(convconfig):                        # converting a bin configuration description into a string of numbers, these strings will be states in the markov chain.
-    str1 = ""                                   # string format: number of bins, followed by a description of each bin. if the item list is [1/4,1/3,1/2],
-                                                #  then 210 means a bin with 2 1/4s, one 1/3, 0 1/2s.
-    str1+=str(convconfig[0]) 
-    for i in range(convconfig[0]):
-        for j in range(len(convconfig[1][i])):
-            str1+=str(convconfig[1][i][j])
-    return str1
-
-def convert(config,L):                          # describing the bin configuration (an array of arrays, i.e., a collection of open bins) in terms of item counts
+def convert(config,L):                          # describing the bin configuration collection (an array of arrays, i.e., a collection of open bins) in terms of item counts
     n = len(config)
     config1 = copy.deepcopy(config)
     config1.sort(key=cmp_to_key(compare))
@@ -34,6 +25,16 @@ def convert(config,L):                          # describing the bin configurati
             arr1[j] = config1[i].count(L[j])
         arr.append(arr1)
     return (n,arr)
+
+def strconv(convconfig):                        # converting a bin configuration description into a string of numbers, these strings will be states in the markov chain.
+    str1 = ""                                   # string format: number of bins, followed by a description of each bin. if the item list is [1/4,1/3,1/2],
+                                                #  then 210 means a bin with 2 1/4s, one 1/3, 0 1/2s.
+    str1+=str(convconfig[0]) 
+    for i in range(convconfig[0]):
+        for j in range(len(convconfig[1][i])):
+            str1+=str(convconfig[1][i][j])
+    return str1
+
 
 
 def BF(config,item,L):                    # simulating best fit for a particular collection of open bins so as to find reachable states, we use this to compute the markov chain
@@ -63,27 +64,6 @@ def BF(config,item,L):                    # simulating best fit for a particular
         config1.sort(key=cmp_to_key(compare))    
         return config1  
 
-def AsymBF(L,p):                              # finds asymptotic performance of BF for the list L with given probabilities p. i.e., returns alpha where alpha*n 
-   n = len(L)                                 # is the expected number of bins used by BF for (L,p)
-   G = ConfigGraphGen(L,p) 
-   newbinedges = NewBinEdges(G)
-   M = ConfigTransitionMatrix(G)
-   m = len(M)
-   val = np.zeros((m))
-   for i in range(m):
-        val[i]  = 0
-   val[m-1] = 1
-   x= np.linalg.lstsq(M, val,rcond=None)     # solves transition eqs. for Markov chain
-      
-   BF = 0
-    
-   for edge in newbinedges:                   #adding up the transitions that create new bins
-        ind1 = edge[0]
-        ind2 = edge[1]
-        BF+= x[0][ind2]*M[ind1][ind2]
-   
-   return BF
-         
 def ConfigGraphGen(L,p):                  # generates the markov chain for the list L with probabilities p
     n = len(L)
     flag = 0
@@ -122,13 +102,13 @@ def ConfigGraphGen(L,p):                  # generates the markov chain for the l
             flag = 1
     return G
 
-def ConfigTransitionMatrix(G):      # generates the markov transition matrix from the markov chain graph
+def ConfigTransitionMatrix(G):      # generates the matrix corresponding to markov chain. Ax = x, M = A - I, need to solve Mx = 000..01
     n = len(G.nodes())
     nodes  = list(G.nodes())
     edges = list(G.edges())  
     M = [ [0] * n for _ in range(n+1)]
     for i in range(n):
-        M[n][i] = 1
+        M[n][i] = 1                 # normalising probabilities to sum to 1
         M[i][i] = -1      
     for edge in edges:
         edgedict = G.get_edge_data(edge[0], edge[1])
@@ -149,12 +129,35 @@ def NewBinEdges(G):                # lists the transitions that create new bins
             arr.append([ind2,ind1])
     return arr  
 
+
+def AsymBF(L,p):                              # finds asymptotic performance of BF for the list L with given probabilities p. i.e., returns alpha where alpha*n 
+   n = len(L)                                 # is the expected number of bins used by BF for (L,p)
+   G = ConfigGraphGen(L,p) 
+   newbinedges = NewBinEdges(G)
+   M = ConfigTransitionMatrix(G)
+   m = len(M)
+   val = np.zeros((m))
+   for i in range(m):
+        val[i]  = 0
+   val[m-1] = 1                              # normalising probabilities to sum to 1
+   x= np.linalg.lstsq(M, val,rcond=None)     # solves transition eqs. for Markov chain
+      
+   BF = 0
+    
+   for edge in newbinedges:                   #adding up the transitions that create new bins
+        ind1 = edge[0]
+        ind2 = edge[1]
+        BF+= x[0][ind2]*M[ind1][ind2]
+   
+   return BF
+         
+
 "-----------------------------------------End of Defining functions-----------------------------------"      
 
     
-print(AsymBF([1/3,1/4], [0.5, 0.5]))     # sample use of code.
-    
-    
+K= [0.245, 0.25, 0.26, 0.27, 0.3, 0.38, 0.46]
+p = [0.2, 0.22, 0.1, 0.16, 0.16, 0.08, 0.08]
+print(AsymBF(K,p))     # sample use of code
     
     
     
